addcf8a489dc92a42cf5ded5b91147a471817c92
SoumyaPraveen <skr.anizham@gmail.com>
2025-02-18 14:16:18 +0530

    Update dbConnection.js

--- dbConnection.js
+++ dbConnection.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose")
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect("mongodb://127.0.0.1:27017/laundry")
 var db=mongoose.connection
 db.on("error", console.error.bind(console, "connection error:")); 
 db.once("open",function(){
